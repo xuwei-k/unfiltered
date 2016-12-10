4e18cc72eb88be9de34638e8575a885b578b28be
xuwei-k <6b656e6a69@gmail.com>
2016-12-10 11:26:06 +0900

    wip

--- docs/src/main/scala/01.scala
+++ docs/src/main/scala/01.scala
@@ -11,4 +11,30 @@ val echo = unfiltered.filter.Planify {
 }
 // #example01
 
+
+// #example02
+unfiltered.jetty.Server.anylocal.plan(echo).run()
+// #example02
+
+
+// #example03
+val echoNice = unfiltered.filter.Planify {
+  case Path(Seg(p :: Nil)) => ResponseString(p)
+  case _ => ResponseString(
+    "I can echo exactly one path element."
+  )
+}
+unfiltered.jetty.Server.anylocal.plan(echoNice).run()
+// #example03
+
+
+// #example04
+val nice = unfiltered.filter.Planify {
+  case _ => ResponseString(
+    "I can echo exactly one path element."
+  )
+}
+unfiltered.jetty.Server.anylocal.plan(echo).plan(nice).run()
+// #example04
+
 }
